2633efba085ba7bca956e1b5b079ed1f884c55c8
Adam Titus and Kimberly Bordon <adam+kimberly@>
2018-01-22 11:37:16 -0800

    create texter thing.

---
+++ Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Texter.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "AC8167b86e17369059cf51285c6c45b7d7";
+        public static string AuthToken = "f6f10a8adc6f01e0e5aa4bc409521a65";
+        public EnvironmentVariables()
+        {
+        }
+    }
+}
